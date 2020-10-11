e22d8a65aad9ee783383aa7234d182abdb8ae941
LaupWing <loc_strict@hotmail.com>
2020-10-11 18:52:03 +0200

    psots

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
    //
    public function show()
    {
+      $posts = Post::where('idposts', 1)->firstOrFail();
+      echo $posts;
       return view('index');
    }
 }
